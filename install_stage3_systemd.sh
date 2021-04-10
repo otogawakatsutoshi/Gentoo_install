7eac8aa7c0f95fc91c3c835ea45d88901eb81993
KatsutoshiOtogawa <gh1ab7im@gmail.com>
2021-04-11 01:58:42 +0900

    update

--- install_stage3_systemd.sh
+++ install_stage3_systemd.sh
@@ -1,8 +1,8 @@
-#/bin/bash
+#!/bin/bash
 
 # [install cookbook](https://wiki.gentoo.org/wiki/Handbook:AMD64)
 
-
+# create partition
 parted -s -a optimal /dev/sda mklabel gpt -- unit mib mkpart primary 1     3 name 1 grub set 1 bios_grub on
 parted -s -a optimal /dev/sda             -- unit mib mkpart primary 3   131 name 2 boot set 2 boot on
 parted -s -a optimal /dev/sda             -- unit mib mkpart primary 131 643 name 3 swap set 3 swap on
@@ -36,48 +36,9 @@ cp /mnt/gentoo/usr/share/portage/config/repos.conf /mnt/gentoo/etc/portage/repos
 # DNS 情報
 cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
 
+# mount
 mount --types proc /proc /mnt/gentoo/proc
 mount --rbind /sys /mnt/gentoo/sys
 mount --make-rslave /mnt/gentoo/sys
 mount --rbind /dev /mnt/gentoo/dev
 mount --make-rslave /mnt/gentoo/dev
-
-chroot /mnt/gentoo /bin/bash
-source /etc/profile
-export PS1="(chroot) ${PS1}"
-
-mount /dev/sda1 /boot
-
-# sync package and source 
-emerge-webrsync
-
-# update all package
-emerge --update --deep --newuse @world
-emerge app-editors/vim
-
-# set timezone 
-echo "Asia/Tokyo" > /etc/timezone
-emerge --config sys-libs/timezone-data
-
-# set locale
-# locale-gen
-
-echo en_US ISO-8859-1 >> /etc/locale.gen
-echo en_US.UTF-8 UTF-8 >> /etc/locale.gen
-echo ja_JP.EUC-JP EUC-JP >> /etc/locale.gen
-echo ja_JP.UTF-8 UTF-8 >> /etc/locale.gen
-echo ja_JP EUC-JP >> /etc/locale.gen
-
-env-update && source /etc/profile && export PS1="(chroot) ${PS1}"
-
-# install kernel
-emerge sys-kernel/gentoo-sources
-
-emerge sys-kernel/genkernel
-
-
-echo "/dev/sda1	/boot	ext2	defaults	0 2"  >> /etc/fstab
-genkernel all
-
-# install firmware
-emerge sys-kernel/linux-firmware
---
+++ install_stage3_systemd_chroot.sh
@@ -0,0 +1,44 @@
+#!bin/bash
+
+# chroot /mnt/gentoo /bin/bash
+
+source /etc/profile
+export PS1="(chroot) ${PS1}"
+
+mount /dev/sda1 /boot
+
+# sync package and source 
+emerge-webrsync
+
+# update all package
+emerge --update --deep --newuse @world
+emerge app-editors/vim
+
+# set timezone 
+echo "Asia/Tokyo" > /etc/timezone
+emerge --config sys-libs/timezone-data
+
+# set locale
+# locale-gen
+
+echo en_US ISO-8859-1 >> /etc/locale.gen
+echo en_US.UTF-8 UTF-8 >> /etc/locale.gen
+echo ja_JP.EUC-JP EUC-JP >> /etc/locale.gen
+echo ja_JP.UTF-8 UTF-8 >> /etc/locale.gen
+echo ja_JP EUC-JP >> /etc/locale.gen
+
+locale-gen
+
+env-update && source /etc/profile && export PS1="(chroot) ${PS1}"
+
+# install kernel
+emerge sys-kernel/gentoo-sources
+
+emerge sys-kernel/genkernel
+
+
+echo "/dev/sda1	/boot	ext2	defaults	0 2"  >> /etc/fstab
+genkernel all
+
+# install firmware
+emerge sys-kernel/linux-firmware
