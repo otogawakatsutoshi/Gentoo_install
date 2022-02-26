1da605af078872017cf32a04a15090f8f0a32918
KatsutoshiOtogawa <13373011+KatsutoshiOtogawa@users.noreply.github.com>
2022-02-26 22:40:57 +0900

    Update install_stage3_systemd.sh

--- install_stage3_systemd.sh
+++ install_stage3_systemd.sh
@@ -98,3 +98,5 @@ mount --rbind /sys /mnt/gentoo/sys
 mount --make-rslave /mnt/gentoo/sys
 mount --rbind /dev /mnt/gentoo/dev
 mount --make-rslave /mnt/gentoo/dev
+mount --bind /run /mnt/gentoo/run
+mount --make-slave /mnt/gentoo/run
