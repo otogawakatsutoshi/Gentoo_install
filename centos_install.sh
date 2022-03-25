91aec9b990a9762f002ab5a577edbe5947db8fa8
KatsutoshiOtogawa <13373011+KatsutoshiOtogawa@users.noreply.github.com>
2022-03-26 03:51:13 +0900

    update

--- centos_install.sh
+++ centos_install.sh
@@ -5,11 +5,15 @@
 
 # dnfはrhel系以外ならarch系にcommunityでインストールできる。
 
-mkdir /mnt/fedora
-mount /dev/sdb4 /mnt/fedora
+mkdir /mnt/centos
+mount /dev/sdb4 /mnt/centos
 
+https://blue-red.ddo.jp/~ao/wiki/wiki.cgi?page=Fedora5+%A4%CE+yum+%A4%CE%A5%EA%A5%DD%A5%B8%A5%C8%A5%EA%A4%F2%CA%D1%B9%B9%A4%B9%A4%EB
+repourl=https://ftp.jp.debian.org/debian
+
+ARCH=amd64
 version=35
-dnf --installroot=/mnt/fedora -releasever=$version centos-stream-release systemd
+dnf --installroot=/mnt/centos -releasever=$version centos-stream-release systemd
 
 # /etc/fstabはdebianは手動で作る方針なので、マルチブートならコピーして流用が簡単
 # マウントするデバイスは帰ること。
